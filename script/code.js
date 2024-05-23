8c0fb0c574fe2bfe7a19127d0e1baca0779dc15b
Name <nizaam01.jeftha@gmail.com>
2024-05-23 09:29:02 +0200

    initial commit

---
+++ script/code.js
@@ -0,0 +1,19 @@
+function greet(name) {
+console.log ("Hello" +" " + name + "!")
+}
+greet("Nizaam")
+
+
+function multiply (num1, num2) {
+    console.log(num1 * num2);
+}
+
+multiply(6, 3);
+
+function tableOfFive(num) {
+let result = num * 5
+console.log (result);
+return result;
+}
+
+tableOfFive(4)
